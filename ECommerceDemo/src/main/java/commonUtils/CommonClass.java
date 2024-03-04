e1203f17e9ce61a99b4c44281440313deec21042
Nandhana1334 <nandhananarayanan13@gmail.com>
2024-03-04 15:01:14 +0530

    Automation-Positive Flow with Report

--- ECommerceDemo/src/main/java/commonUtils/CommonClass.java
+++ ECommerceDemo/src/main/java/commonUtils/CommonClass.java
@@ -1,15 +1,19 @@
 package commonUtils;
 
+import java.io.File;
 import java.io.FileInputStream;
 
+
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.Properties;
 import org.json.JSONObject;
 import java.nio.file.Files;
+
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -17,20 +21,41 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.BeforeTest;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
 
-//import com.google.common.io.Files;
+import com.aventstack.extentreports.ExtentReports;
+
+import com.aventstack.extentreports.*;
+
+import org.testng.annotations.BeforeTest;
+import com.aventstack.extentreports.reporter.*;
+
+import com.aventstack.extentreports.reporter.configuration.ViewName;
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
 
 public class CommonClass {
 	
+	
+	
 	protected static WebDriver driver;
 	 static Properties prop;
+	 
 	 @BeforeTest
 	   public static void setup() 
 	   {
@@ -61,12 +86,32 @@ public class CommonClass {
 			driver.manage().window().maximize();
 			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 			driver.get(prop.getProperty("url"));
+			
 	   }
 
-	
+	    protected ExtentReports extent;
+	    protected ExtentTest test;
 
-	 
-	
+	    @BeforeTest
+	    public void setupExtentReport() {
+	        
+	    	//ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter("extentReport.html");
+	    	ExtentSparkReporter htmlReporter = new ExtentSparkReporter("extentReport.html");
+	        extent = new ExtentReports();
+	        extent.attachReporter(htmlReporter);
+	    }
+
+
+	  public void validLogin()
+
+	   {
+		   WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+		   String loginLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
+		   type("loginData",locateElement(loginLocatorPath,"emailField", "xpath"), "username");
+		   type("loginData",locateElement(loginLocatorPath,"passwordField", "xpath"),"password");
+		   clickElement(locateElement(loginLocatorPath,"loginButton", "xpath"));
+		   Assert.assertEquals(locateElement(loginLocatorPath,"signoutButton", "xpath").getText(), "Sign Out");
+	 }
 	public  static String readCredentials(String excelSheet,String credentials)
 	{
 		String name="";
@@ -83,14 +128,17 @@ public class CommonClass {
 
 		XSSFSheet loginSheet=workbook.getSheet(excelSheet);
 		int numRows=loginSheet.getLastRowNum();
-		
+		//System.out.println(numRows);
 		for(int i=0;i<numRows+1;)
 		{
 	
 			XSSFRow row=loginSheet.getRow(i);
 			if(row.getCell(0).getStringCellValue().equalsIgnoreCase(credentials))
 			{
+				
 				name=row.getCell(1).getStringCellValue();
+				
+
 			}
 			i=i+1;
 		}
@@ -103,7 +151,7 @@ public class CommonClass {
 	
 	public static void type(String excelSheet,WebElement element,String credentials)
 	{
-		element.sendKeys(CommonClass.readCredentials( excelSheet,credentials));
+		element.sendKeys(readCredentials( excelSheet,credentials));
 	}
 	public static void clickElement(WebElement element)
 	{
@@ -116,11 +164,28 @@ public class CommonClass {
 		return element.getText();
 		
 	}
+	public static void clearField(WebElement element)
+	{
+		element.clear();
+	}
+	public boolean productDisplayed(WebElement element)
+	{
+		boolean value = element.isDisplayed();
+		return value;
+	}
    
-
+    public void threadWait()
+    {
+    	try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
 	
 	 public static String getLocatorFromJson(String path,String locator,String locatorType) {
-	        //String path = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
+	
 	        String jsonData = null;
 	        String value = null;
 
@@ -134,6 +199,7 @@ public class CommonClass {
 	            try {
 	                JSONObject jsonObject = new JSONObject(jsonData);
 	                JSONObject emailField = jsonObject.getJSONObject(locator);
+	              
 
 	                if (locatorType.equalsIgnoreCase("id")) {
 	                   value= emailField.getString("id");
@@ -141,7 +207,11 @@ public class CommonClass {
 	                    value= emailField.getString("cssSelector");
 	                } else if (locatorType.equalsIgnoreCase("xpath")) {
 	                   value= emailField.getString("xpath");
-	                } else {
+	                } 
+	                else if (locatorType.equalsIgnoreCase("name")) {
+		                   value= emailField.getString("name");
+		                } 
+	                else {
 	                   value= null; 
 	                }
 	            } catch (Exception e) {
@@ -156,106 +226,12 @@ public class CommonClass {
 	        return value;
 	    }
 
-//	 public static String pwdLocator(String locatorType) {
-//	        String path = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
-//
-//	        try {
-//	            
-//	            String jsonString = new String(Files.readAllBytes(Paths.get(path)));
-//	            
-//	            
-//	            JSONObject jsonObject = new JSONObject(jsonString);
-//	            
-//	            
-//	            JSONObject pwdField = jsonObject.getJSONObject("passwordField");
-//
-//	            
-//	            if (locatorType.equalsIgnoreCase("id")) {
-//	                return pwdField.getString("id");
-//	            } else if (locatorType.equalsIgnoreCase("xpath")) {
-//	                return pwdField.getString("xpath");
-//	            } else {
-//	                return null;
-//	            }
-//	        } catch (IOException e) {
-//	            e.printStackTrace();
-//	            return null;
-//	        }
-//	    }
-//	 
-//	 public static String loginBtnLocator(String locatorType) {
-//	        String path = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
-//
-//	        try {
-//	            String jsonString = new String(Files.readAllBytes(Paths.get(path)));
-//	            JSONObject jsonObject = new JSONObject(jsonString);
-//	            JSONObject loginBtn = jsonObject.getJSONObject("loginButton");
-//
-//	            switch (locatorType.toLowerCase()) {
-//	                case "id":
-//	                    return loginBtn.getString("id");
-//	                case "cssselector":
-//	                    return loginBtn.getString("cssSelector");
-//	                case "xpath":
-//	                    return loginBtn.getString("xpath");
-//	                case "name":
-//	                    return loginBtn.getString("name");
-//	                default:
-//	                    return null;
-//	            }
-//	        } catch (IOException e) {
-//	            e.printStackTrace();
-//	            return null;
-//	        }
-//	    }
-//	 public static String loginBtnLocator(String locatorType) {
-//	        String path = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
-//
-//	        try {
-//	            String jsonString = new String(Files.readAllBytes(Paths.get(path)));
-//	            JSONObject jsonObject = new JSONObject(jsonString);
-//	            JSONObject loginBtn = jsonObject.getJSONObject("loginButton");
-//
-//	            if (locatorType.equalsIgnoreCase("id")) {
-//	                return loginBtn.getString("id");
-//	            } else if (locatorType.equalsIgnoreCase("cssSelector")) {
-//	                return loginBtn.getString("cssSelector");
-//	            } else if (locatorType.equalsIgnoreCase("xpath")) {
-//	                return loginBtn.getString("xpath");
-//	            } else if (locatorType.equalsIgnoreCase("name")) {
-//	                return loginBtn.getString("name");
-//	            } else {
-//	                return null;
-//	            }
-//	        } catch (IOException e) {
-//	            e.printStackTrace();
-//	            return null;
-//	        }
-//	    }
-//
-//	    public static String signoutBtnLocator(String locatorType) {
-//	        String path = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
-//
-//	        try {
-//	            String jsonString = new String(Files.readAllBytes(Paths.get(path)));
-//	            JSONObject jsonObject = new JSONObject(jsonString);
-//	            JSONObject signoutBtn = jsonObject.getJSONObject("signoutButton");
-//
-//	            if (locatorType.equalsIgnoreCase("xpath")) {
-//	                return signoutBtn.getString("xpath");
-//	            } else {
-//	                return null;
-//	            }
-//	        } catch (IOException e) {
-//	            e.printStackTrace();
-//	            return null;
-//	        }
-//	    }
 
 	public static WebElement locateElement(String path,String element,String locatorType)
 	{
 		
 		String locator =getLocatorFromJson(path,element, locatorType);
+		
 		WebElement element1 =null;
 		switch(locatorType)
 		{
@@ -281,23 +257,48 @@ public class CommonClass {
 		}
 		return element1;
 	}
+	public static String getScreenshotPath(WebDriver driver)
+	{
+		TakesScreenshot screen=(TakesScreenshot)driver;
+		File src=screen.getScreenshotAs(OutputType.FILE);
+		String srcFile=System.getProperty("user.dir")+"/screenshots/"+System.currentTimeMillis()+".png";
+		File dest=new File(srcFile);
+	    try
+		{
+			FileUtils.copyFile(src, dest);		
+		}catch(IOException e)
+	    {
+			e.printStackTrace();
+	    }
 	
-//	public static void actions(WebElement element ,String input,String credentials)
-//	{
-//		
-//		switch(input)
-//		{
-//		case "click":
-//			element.click();
-//		case "getText":
-//			element.getText();
-//		case "sendkeys":
-//			element.sendKeys(CommonClass.readCredentials(credentials));
-//		
-//			
-//		}
-		
-		
+	return srcFile;
+	}
+	 @AfterMethod
+	    public void getResult(ITestResult result) {
+	        
+	        if (result.getStatus() == ITestResult.FAILURE) {
+	            test.fail("Test Case Failed is " + result.getName());
+	            test.fail("Test Case Failed is " + result.getThrowable());
+	            String strPath=getScreenshotPath(driver);
+	    	    test.fail(MediaEntityBuilder.createScreenCaptureFromPath(strPath).build());
+	        } else if (result.getStatus() == ITestResult.SUCCESS) {
+	            test.pass("Test Case Passed is " + result.getName());
+	        } else if (result.getStatus() == ITestResult.SKIP) {
+	            test.skip("Test Case Skipped is " + result.getName());
+	        }
+	    }
+
+	    @AfterTest
+	    public void tearDown() {
+	      
+	        extent.flush();
+	    }
+	@AfterTest
+	public static void terminate()
+	{
+		driver.close();
+	}
+
 		
 		
 	}
--- ECommerceDemo/src/test/java/testscripts/TestScenarios.java
+++ ECommerceDemo/src/test/java/testscripts/TestScenarios.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,6 +17,7 @@ import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -25,70 +27,75 @@ import org.testng.annotations.Test;
 import commonUtils.CommonClass;
 
 public class TestScenarios extends CommonClass {
-	
-		
-
-		  
-		  
-		  
-		   @Test
-		   public void validLogin()
-
+		@Test
+		   public void addToCartTest() 
 		   {
-			   WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
-			   //locateElement("email", "xpath").sendKeys(null);
-			   String loginLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//login.json";
 			   
-			   type("loginData",locateElement(loginLocatorPath,"emailField", "xpath"), "username");
-			   type("loginData",locateElement(loginLocatorPath,"passwordField", "xpath"),"password");
-			   clickElement(locateElement(loginLocatorPath,"loginButton", "xpath"));
-			   Assert.assertEquals(locateElement(loginLocatorPath,"signoutButton", "xpath").getText(), "Sign Out");
-			   
-
-		   }
-		   @Test
-		   public void addToCart() throws InterruptedException
-		   {
-		  
+			  test = extent.createTest("Add To Cart Test", "Verify adding product to cart functionality");
+			  validLogin();
 			   String productLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//products.json";
-			   Thread.sleep(2000);
-			   locateElement(productLocatorPath,"search", "xpath").click();
-			   type("ProductData", locateElement(productLocatorPath,"search", "xpath"), "ProductName");
-			   			   
-			   clickElement(locateElement(productLocatorPath,"viewButton", "xpath"));
-			   clickElement(locateElement(productLocatorPath,"cartButtonA", "xpath"));
-			   clickElement(locateElement(productLocatorPath,"navigateToCart", "xpath"));
-			  // clickElement(locateElement(productLocatorPath,"cartButton", "xpath"));
-			  // Assert.assertEquals(locateElement(productLocatorPath,"chkHeading", "xpath").getText(),"ZARA COAT 3");
-			   Assert.assertEquals(locateElement(productLocatorPath,"chckCart", "xpath"),"ZARA COAT 3");
-
+			   if(productDisplayed(locateElement(productLocatorPath,"homepageProduct", "xpath")))
+			   {
+				   clickElement(locateElement(productLocatorPath,"viewButton", "xpath"));
+			   }
+			   else
+			   {
+				   System.out.println("Product Not Available");
+			   }
+			   threadWait();	   
+			   clickElement(locateElement(productLocatorPath,"cartButton", "xpath"));
+			   Assert.assertEquals(getTextOfElement(locateElement(productLocatorPath,"chkHeading", "xpath")),readCredentials( "ProductData","ProductName19"));
 		   }
+		   
+		   @Test
+		   public void cartDisplayTest()
+		   {
+			   test = extent.createTest("Cart Display Test", "Verify the product displayed in the cart");
+			   String cartLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//cart.json";
+			   clickElement(locateElement(cartLocatorPath,"navigateToCart", "xpath"));
+			   Assert.assertEquals(getTextOfElement(locateElement(cartLocatorPath,"chckNameInCart", "xpath")),readCredentials( "ProductData","ProductName1"));
+
+	    
+			 }
+		   
 
 		   
 		   @Test
-		   public void checkout()
+		   public void checkoutTest()
 		   {
+			   
+			   test = extent.createTest("Checkout Test", "Verify the checkout functionality");
 			   String orderLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//orderLocators.json";
-
 			   clickElement(locateElement(orderLocatorPath,"checkoutButton", "xpath"));
-			   type("OrderData", locateElement(orderLocatorPath,"countryDropdown", "xpath"), "Country");
+			
+			   clearField(locateElement(orderLocatorPath, "email", "xpath"));
+			   type("loginData", locateElement(orderLocatorPath, "email", "xpath"), "username");
+			   type("OrderFormData", locateElement(orderLocatorPath, "name", "xpath"), "name");
+			   clearField(locateElement(orderLocatorPath, "creditCardNo", "xpath"));
+			   type("OrderFormData", locateElement(orderLocatorPath, "creditCardNo", "xpath"), "Credit Card Number");
+			   type("OrderFormData", locateElement(orderLocatorPath, "cvv", "xpath"), "cvv");
+			
+			   clickElement(locateElement(orderLocatorPath,"countryDropdown", "xpath"));
+			   type("OrderFormData", locateElement(orderLocatorPath,"countryDropdown", "xpath"), "Country");
 			   clickElement(locateElement(orderLocatorPath,"countryName", "xpath"));
-			   Assert.assertEquals(locateElement(orderLocatorPath,"chckHeading", "xpath"),"Thankyou for the order.");
+			   clickElement(locateElement(orderLocatorPath,"placeOrder", "xpath"));
+			   
+			   Assert.assertEquals(getTextOfElement(locateElement(orderLocatorPath,"chckHeading", "xpath")),"THANKYOU FOR THE ORDER.");
 			   
 		   }
 		   @Test
-		   public void placeOrder()
+		   public void placeOrderTest()
 		   {
+			   test = extent.createTest("Place Order Test", "Verify the placeorder functionality");
 			   String orderLocatorPath = System.getProperty("user.dir") + "//src//test//resources//locators//orderLocators.json";
-			   clickElement(locateElement(orderLocatorPath,"placeOrder", "xpath"));
 			   clickElement(locateElement(orderLocatorPath,"orderButton", "xpath"));
+			   String orderId = locateElement(orderLocatorPath,"OrderId1", "xpath").getText();
 			   clickElement(locateElement(orderLocatorPath,"viewSummary", "xpath"));
-			   Assert.assertEquals(readCredentials("OrderData", "OrderId"), "65dec204a86f8f74dc8a1c16");
+			  
+			   Assert.assertEquals(orderId,getTextOfElement(locateElement(orderLocatorPath,"OrderId2", "xpath")));
 			   
 		   }
-		   
-		   
-		   
+	   
 		   
 		   
 		   
